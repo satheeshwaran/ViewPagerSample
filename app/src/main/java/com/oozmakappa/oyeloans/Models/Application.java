319203d5d26698fe68a3f8f799caa0ee767ce45c
sricharan.c <ytogiveup@gmail.com>
2016-09-18 18:19:22 +0530

    new file:   app/src/main/java/com/oozmakappa/oyeloans/Models/Application.java

---
+++ app/src/main/java/com/oozmakappa/oyeloans/Models/Application.java
@@ -0,0 +1,38 @@
+package com.oozmakappa.oyeloans.Models;
+
+/**
+ * Created by sricharan.c on 18-09-2016.
+ */
+public class Application {
+    public String applicationID;
+
+    public String loanAmount;
+    public String loanDuration;
+
+    public String firstName;
+    public String lastName;
+    public String mobile;
+    public String dob;
+    public String emailID;
+    public String gender;
+
+    public String panNumber;
+    public String aadharNumber;
+
+    public String buildingNumber;
+    public String buildingName;
+    public String streetName;
+    public String city;
+    public String pinCode;
+
+    public String highestQualification;
+    public String educationalInstitution;
+
+    public String employmentType;
+    public String employerName;
+    public String occupation;
+    public String employerPhone;
+    public String monthlyIncome;
+    public String firstPayDate;
+
+}
