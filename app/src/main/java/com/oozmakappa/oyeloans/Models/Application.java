98a431f67798acfff2aa6fab033f74c42b45050e
sricharan.c <ytogiveup@gmail.com>
2016-09-18 19:07:35 +0530

    modified:   app/src/main/java/com/oozmakappa/oyeloans/Models/Application.java

--- app/src/main/java/com/oozmakappa/oyeloans/Models/Application.java
+++ app/src/main/java/com/oozmakappa/oyeloans/Models/Application.java
@@ -9,30 +9,8 @@ public class Application {
     public String loanAmount;
     public String loanDuration;
 
-    public String firstName;
-    public String lastName;
-    public String mobile;
-    public String dob;
-    public String emailID;
-    public String gender;
+    public LoanUser loanUserObject;
 
-    public String panNumber;
-    public String aadharNumber;
-
-    public String buildingNumber;
-    public String buildingName;
-    public String streetName;
-    public String city;
-    public String pinCode;
-
-    public String highestQualification;
-    public String educationalInstitution;
-
-    public String employmentType;
-    public String employerName;
-    public String occupation;
-    public String employerPhone;
-    public String monthlyIncome;
     public String firstPayDate;
 
 }
