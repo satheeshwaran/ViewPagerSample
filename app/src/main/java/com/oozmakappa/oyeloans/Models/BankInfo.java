c511bebe974c10c1d44ab2d0b16ccfcba8eab62f
sricharan.c <ytogiveup@gmail.com>
2016-09-23 01:01:33 +0530

    added the bankinfo to applicationapp/src/main/java/com/oozmakappa/oyeloans/Models/Application.java

---
+++ app/src/main/java/com/oozmakappa/oyeloans/Models/BankInfo.java
@@ -0,0 +1,12 @@
+package com.oozmakappa.oyeloans.Models;
+
+/**
+ * Created by sricharan.c on 22-09-2016.
+ */
+public class BankInfo {
+
+    public String ifscCode;
+    public String bankAddress1;
+    public String bankAddress2;
+
+}
---
+++ app/src/main/java/com/oozmakappa/oyeloans/Models/Loan.java
@@ -0,0 +1,8 @@
+package com.oozmakappa.oyeloans.Models;
+
+/**
+ * Created by sricharan.c on 23-09-2016.
+ */
+public class Loan {
+    public int loanID;
+}
